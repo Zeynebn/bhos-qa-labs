4bb0dfd18bd8501ae86231cdefad858eb2c44ad5
Zeynebn <64706156+Zeynebn@users.noreply.github.com>
2021-12-14 17:02:26 +0400

    Update ApiUrlConstants.java

--- src/test/java/com/example/springproj6/ApiUrlConstants.java
+++ src/test/java/com/example/springproj6/ApiUrlConstants.java
@@ -3,6 +3,6 @@ package com.example.springproj6;
 
 public class ApiUrlConstants {
     public final static String FIREBASE_SIGNIN_EMAIL_AND_PASSWORD = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
-    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/projectspring-e6266/databases/(default)/documents/users";
+    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/projectspring-e6266/databases/(default)/documents/user";
     public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/projectspring-e6266.appspot.com/o";
 }
